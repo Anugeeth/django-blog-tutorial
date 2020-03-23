1f3cc2f1cd820b7b041cadf37ed66c40f7591945
anugeeth <anugeeth2@gmail.com>
2020-03-23 16:54:34 +0530

    added forms in user app

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import UserRegistrationForm as forms
 
 from django.http import HttpResponse
 # Create your views here.
@@ -9,18 +8,21 @@ from django.http import HttpResponse
 
 def register(request):
 
-# checking whether is request is a post request or not if yes data is posted to the user model
+# checking whether is request is a post request or not if yes data is [passed] to the usercreation model
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = forms(request.POST)
         if form.is_valid():
+            # saving form
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
 
             # for returning messages 
             return redirect('blog-home')
+
 # for other requests on the same route user creation form is returned  
     else:
-        form = UserCreationForm()
+        form = forms()
 
     return render(request, 'users/register.html', {'form' : form})
     
