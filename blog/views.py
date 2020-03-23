3f504f44ebb9bc9262dafcd2e88df432ba74223d
anugeeth <anugeeth2@gmail.com>
2020-03-23 14:43:43 +0530

    added templates

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+posts = [
+    {
+        'author': 'Anugeeth',
+        'title': 'test post',
+        'content': 'Test conent',
+        'date_posted': '12/12/12'
+    },
+    {
+        'author': 'tester',
+        'title': 'test pdsdsdost',
+        'content': 'Test dssdsdsdsDasdconent',
+        'date_posted': '12/12/12'
+
+    },
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html')
