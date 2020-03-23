df314e3b8d66a0000270534aa298e8b090c7ead0
anugeeth <anugeeth2@gmail.com>
2020-03-23 16:45:09 +0530

    added regiser view

--- users/views.py
+++ users/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+from django.http import HttpResponse
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm()
+
+# checking whether is request is a post request or not if yes data is posted to the user model
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            # for returning messages 
+            return redirect('blog-home')
+# for other requests on the same route user creation form is returned  
+    else:
+        form = UserCreationForm()
+
     return render(request, 'users/register.html', {'form' : form})
+    
+def login(request):
+    # form = UserCreationForm()
+    # return render(request, 'users/register.html', {'form' : form})
+    return HttpResponse('<h1>Login</h1>')
